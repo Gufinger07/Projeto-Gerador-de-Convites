d0514b708cd05992cc75b19fa556fea36e8f2be8
Gufinger07 <gusti.cf@gmail.com>
2020-12-10 19:34:23 -0300

    Adicionando os primeiros scripts

---
+++ Projeto_Convite/index.js
@@ -0,0 +1,47 @@
+var botaoAdicionar = document.querySelector("#adicionar-convidado");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var convidado = obtemconvidadoDoFormulario(form);
+
+    adicionaPacienteNaTabela(convidado);
+
+    form.reset();
+
+    
+
+})
+function obtemconvidadoDoFormulario(form) {
+
+    var convidado = {
+        nome: form.nome.value,
+        idade: form.idade.value
+    }
+    
+    return convidado;
+}
+function montaTr(convidado) {
+    var convidadoTr = document.createElement("tr");
+    convidadoTr.classList.add("convidado");
+
+    convidadoTr.appendChild(montaTd(convidado.nome, "info-nome"));
+    convidadoTr.appendChild(montaTd(convidado.idade, "info-idade"));
+    
+
+    return convidadoTr;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
+}
+function adicionaPacienteNaTabela(convidado) {
+    var convidadoTr = montaTr(convidado);
+    var tabela = document.querySelector("#tabela-convidados");
+    tabela.appendChild(convidadoTr);
+}
