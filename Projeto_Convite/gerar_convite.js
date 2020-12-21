1b3fb6c077e86e80145cca9a73d2761912e79f30
Gufinger07 <gusti.cf@gmail.com>
2020-12-21 19:28:43 -0300

    Adicionando validação e erro

--- Projeto_Convite/gerar_convite.js
+++ Projeto_Convite/gerar_convite.js
@@ -1,40 +1,49 @@
 var botao_gerar = document.querySelector("#gerar-convite");
 botao_gerar.addEventListener("click", function(event) {
     event.preventDefault();
-    
+
     var nome_no_convite = document.querySelector(".nome-convite")
     var novo_nome = document.querySelector("#aniversariante")
-    nome_no_convite.textContent = novo_nome.value
-
+    
+   
     var idade_no_convite = document.querySelector("#idade-mudavel")
     var nova_idade = document.querySelector("#idade")
-    idade_no_convite.textContent = nova_idade.value
+    
 
     var data_no_convite = document.querySelector(".data-convite")
     var nova_data = document.querySelector('input[type="date"]')
-    data_no_convite.textContent = nova_data.value
+    
     
 
 
     var hora_no_convite = document.querySelector(".hora-convite")
     var nova_hora = document.querySelector('input[type="time"]')
-    hora_no_convite.textContent = nova_hora.value
+    
 
     var local_no_convite = document.querySelector(".local-convite")
     var novo_local = document.querySelector("#local")
-    local_no_convite.textContent = novo_local.value
+    
     
     var radios = document.getElementsByName("opcao")
 
-    
+    if(novo_nome.value.length == 0 || nova_idade.value.length == 0 || nova_data.value.length == 0 || nova_hora.value.length == 0 || novo_local.value.length == 0) {
+        mostra_erro()
+        return
+    } else {
+        nome_no_convite.textContent = novo_nome.value
+        idade_no_convite.textContent = nova_idade.value
+        data_no_convite.textContent = nova_data.value
+        hora_no_convite.textContent = nova_hora.value
+        local_no_convite.textContent = novo_local.value
+
+
+    }
     if (radios[0].checked) {
         adiciona_classe_menino()
-        console.log("Escolheu: Menino" );  
         return
     }
     if(radios[1].checked) {
         adiciona_classe_menina()
-        console.log("Escolheu: Menina")
         return
     }
 })
@@ -49,4 +58,8 @@ function adiciona_classe_menina() {
     var classe_menina = document.getElementById("fazendo-convite")
     classe_menina.classList.remove("convite-menino")
     classe_menina.classList.add("convite-menina")
+}
+function mostra_erro() {
+    var erro = document.querySelector(".mensagem-erro")
+    erro.innerHTML = "Verifique os dados!!"
 }
